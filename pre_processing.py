99c36e4c01ad237aff9c63d4de02749cc560ef45
dtxwhzw <10976872@qq.com>
2020-01-02 17:58:30 +0800

    add some libraries and steps for classification's pre-processing

--- pre_processing.py
+++ pre_processing.py
@@ -1,8 +1,12 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import pylab as pl
+import itertools
 import numpy as np
-%matplotlib inline#if you'll run this code in jupyter notebook, then this line is needed, otherwise you can comment it.
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+#%matplotlib inline if you'll run this code in jupyter notebook, then this line is needed, otherwise you can comment it.
 
 '''
 import the data or download the data here
@@ -34,3 +38,28 @@ test = df[~msk]
 """
 if we are going to do linear regression, the pre_procession part ends here.
 """
+
+df['custcat'].value#this will return each value of the given colunm
+df['custcat'].value_counts()#this will return the number of each value in a column
+df.hist(column='income', bins=50)
+'''this will draw a histogram use the data in the specific column, and the bins equals to the number of blocks in the plot'''
+
+'''
+Normalize Data
+Data Standardization give data zero mean and unit variance, it is good practice, 
+especially for algorithms such as KNN which is based on distance of cases:
+'''
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X[0:5]
+'''
+Train Test Split
+'''
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+'''
+the column number of x is equal to y, and the random_state have the same effect as random.seed(), it is used to 
+ensure the dataset is split into a same one. otherwise the output will be different everytime you run the code.
+'''
+'''this is the end of pre-processing of knn classification'''
